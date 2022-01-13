6c6bc1796f7d920436c398fb5377226c41850ce7
Mateusz Mazurek <mauzer.official.1@gmail.com>
2022-01-13 21:29:55 +0100

    Initial commit

---
+++ Equation.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+
+
+class Equation(tk.PanedWindow):
+    def __init__(this, master):
+        super().__init__(master)
+
+        this.value = tk.Entry(this)
+        this.a = tk.Entry(this)
+        this.b = tk.Entry(this)
+        this.c = tk.Entry(this)
+
+        this.label_eq = tk.Label(this, text=" = (")
+        this.label_a = tk.Label(this, text=") + (")
+        this.label_b = tk.Label(this, text=") + (")
+        this.label_c = tk.Label(this, text=")")
+
+        this.value.pack(side=tk.LEFT)
+        this.label_eq.pack(side=tk.LEFT)
+        this.a.pack(side=tk.LEFT)
+        this.label_a.pack(side=tk.LEFT)
+        this.b.pack(side=tk.LEFT)
+        this.label_b.pack(side=tk.LEFT)
+        this.c.pack(side=tk.LEFT)
+        this.label_c.pack(side=tk.LEFT)
+
+    @property
+    def values(self):
+        value = self.value.get()
+        a = int(self.a.get())
+        b = int(self.b.get())
+        c = int(self.c.get())
+        assert a * a + b * b + c * c > 0, "At Least One Of Values Has To Be Set"
+        return Eq({
+            'v': value, 'a': a, 'b': b, 'c': c
+        })
+
+
+class Eq:
+    a: int
+    b: int
+    c: int
+    v: int
+
+    def __init__(self, d: dict):
+        for key, val in d.items():
+            self.__setattr__(key, int(val))
---
+++ main.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+from tkinter.messagebox import showinfo
+from Equation import Equation
+
+
+class App(tk.Tk):
+    def __init__(this, title=""):
+        super().__init__()
+        this.title = title
+        this.calc = tk.Button(this, command=this.calculate, text="Click Me!")
+
+        this.first = Equation(this)
+        this.first.pack()
+
+        this.second = Equation(this)
+        this.second.pack()
+
+        this.third = Equation(this)
+        this.third.pack()
+
+        this.calc.pack()
+
+        this.answer = tk.StringVar()
+        this.answer_label = tk.Label(textvariable=this.answer)
+        this.answer_label.pack()
+
+
+    def run(this):
+        this.mainloop()
+
+    def calculate(this):
+        x1, x2, x3 = this.first.values, this.second.values, this.third.values
+        assert x1 and x2 and x3, "Unable To Parse With Zeros"
+        # nfl - kgn + ogj - old - pfj + pkg
+        # ---------------------------------
+        # rfl - kgr + gmo - lho - fmp + khp
+        c =\
+            (
+                (x3.v * x1.a * x2.b) - (x2.a * x1.b * x3.v) + (x1.b * x2.v * x3.a) -
+                (x3.a * x2.b * x1.v) - (x3.b * x1.a * x2.v) + (x3.b * x2.a * x1.b)
+            ) /\
+            (
+                (x3.c * x1.a * x2.b) - (x2.a * x1.b * x3.c) + (x1.b * x2.c * x3.a) -
+                (x3.a * x1.c * x2.b) - (x3.b * x1.a * x2.c) + (x3.b * x2.a * x1.c)
+            )
+        # fj - kd + c * (kh - fm)
+        # -----------------------
+        # fl - kg
+        b =\
+            (
+                (x1.a * x2.v - x2.a * x1.v) + (c * (x2.a * x1.c - x1.a * x2.c))
+            ) /\
+            (
+                x1.a * x2.b - x2.a * x1.b
+            )
+        # d - g*b - h*c
+        # -------------
+        # f
+        a =\
+            (
+               x1.v - x1.b * b - x1.c * c
+            ) / x1.a
+
+        this.answer.set(f"{a=}, {b=}, {c=}")
+
+
+if __name__ == '__main__':
+    App("Calculate Equations").run()
